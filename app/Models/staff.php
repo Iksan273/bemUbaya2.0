72ad4a961c38a96048aed46e8577d0640c416cad
Muhammad Ikhsan <iksan8991@gmail.com>
2022-09-06 01:39:21 +0700

    update staff model

--- app/Models/staff.php
+++ app/Models/staff.php
@@ -1,11 +1,37 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class staff extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+    protected $fillable = [
+        'nama',
+        'nrp',
+        'fakultas',
+        'pilihan1',
+        'alasan1',
+        'pilihan2',
+        'alasan2',
+        'idline',
+        'nohp',
+        'cv',
+        'ktm',
+        'fotodiri',
+        'portofolio',
+        'essay',
+        'jadwal1',
+        'jadwal2',
+        'jadwal3',
+        'jadwal4',
+        'jadwal5',
+        'jadwal6',
+        'jadwal7',
+        'jadwal8',
+        'jadwal9',
+    ];
 }
